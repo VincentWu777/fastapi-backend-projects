9ab7a2b90f7fa2ad0239e2be6d93e7dd6f4ad691
vid-ubuntu <vidwuplus@gmail.com>
2026-01-29 21:52:46 -0800

    schemas

---
+++ 01_pydantic_contracts/app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from pydantic.networks import EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+    age: int | None = Field(default=None, ge=0, le=130)
+
+class UserPublic(BaseModel):
+    id: int
+    email: EmailStr
+    age: int | None = None
+
